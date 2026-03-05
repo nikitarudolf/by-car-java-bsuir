af178be93eb0b0e2ceaa89bc6be40713b15f85a5
Nikita Rudolf <nikitosr423@gmail.com>
2026-03-05 14:18:25 +0300

    chore

--- car-service/src/main/java/by/bycar/car_service/model/User.java
+++ car-service/src/main/java/by/bycar/car_service/model/User.java
@@ -1,6 +1,14 @@
 package by.bycar.car_service.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
