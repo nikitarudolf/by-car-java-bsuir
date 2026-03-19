497c33772c6575b72e5dfa6380dbd200f94b9252
Nikita Rudolf <nikitosr423@gmail.com>
2026-03-19 14:09:35 +0300

    chore

--- src/main/java/by/bycar/carservice/repository/AdvertisementRepository.java
+++ src/main/java/by/bycar/carservice/repository/AdvertisementRepository.java
@@ -21,7 +21,9 @@ public interface AdvertisementRepository extends JpaRepository<Advertisement, Lo
             + "JOIN c.model m " + "JOIN m.brand b " + "WHERE b.name = :brandName AND a.price <= :maxPrice ")
     Page<Advertisement> findAllByBrandAndPriceJPQL(String brandName, Double maxPrice, Pageable pageable);
 
-    @Query(value = "SELECT a.* FROM advertisement a " + "JOIN cars c ON a.car_id = c.id " + "JOIN models m ON c.model_id = m.id " + "JOIN brands b ON m.brand_id = b.id " + "WHERE b.name = :brandName AND a.price <= :maxPrice",
+    @Query(value = "SELECT a.* FROM advertisement a " + "JOIN cars c ON a.car_id = c.id "
+            + "JOIN models m ON c.model_id = m.id " + "JOIN brands b ON m.brand_id = b.id "
+            + "WHERE b.name = :brandName AND a.price <= :maxPrice",
             nativeQuery = true)
     Page<Advertisement> findByBrandAndPriceNative(String brandName, Double maxPrice, Pageable pageable);
 
