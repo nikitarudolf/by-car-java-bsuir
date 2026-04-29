bfce5ce11b0b9c178a3a03de7cc9019ffb842985
Nikita Rudolf <nikitosr423@gmail.com>
2026-04-30 01:48:07 +0300

    deploy

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 
+const API_BASE_URL = process.env.REACT_APP_API_URL || '/api';
+
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
